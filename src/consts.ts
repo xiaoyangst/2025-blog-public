567421f9f110926477970b51a47a93619d2a1fcd
糖果罐里 <96154468+xiaoyangst@users.noreply.github.com>
2025-12-05 10:09:55 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -7,8 +7,8 @@ export const CARD_SPACING_SM = 24
  * GitHub 仓库配置
  */
 export const GITHUB_CONFIG = {
-	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'yysuni',
+	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'xiaoyangst',
 	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2025-blog-public',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
-	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '-'
+	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '2412982'
 } as const
